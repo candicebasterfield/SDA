dc2b1a6f6585611d907a75437c2619ff90ee7d53
candicebasterfield <candice101101@gmail.com>
2024-09-06 09:25:00 -0400

    Your descriptive commit message here

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SDA
 Software development web app
+
+
+This project is a tool designed to simulate random events, providing users with interactive visualizations and an easy-to-use dashboard interface. 
+
+The tool uses Streamlit to create a web-based interface and Plotly Express for generating visualizations like histograms and scatter plots.
+
+ It is developed in Python and intended to be run locally on a user's machine.
---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv', sep=',')  # Ensure this matches the dataset used in EDA
+print(df)
+
+st.header("Exploratory Data Analysis")
+# Histogram of car prices
+fig = px.histogram(df, x='price', title='Histogram of Car Prices')  
+st.plotly_chart(fig)
+
+#Scatterplot
+fig = px.scatter(df, x='odometer', y='price', title='Scatterplot of Price vs. Odometer')  
+st.plotly_chart(fig)
+
+if st.checkbox('Show Histogram'):
+    fig = px.histogram(df, x='model_year')
+    st.plotly_chart(fig)
+
+
+
